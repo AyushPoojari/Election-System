8805c9730193475e7570eb67cfd3b50a1d0548e0
Ayush Poojari <ayushp.b11@gmail.com>
2022-08-01 21:17:42 +0530

    Added Election.sol

---
+++ contracts/Election.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.5.16;
+
+contract Election {
+    //Candidate String
+    //string public candidate;
+    uint256 public candidatesCount;
+
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+
+    mapping(uint256 => Candidate) public candidates;
+
+    //Constructor
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) public {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+}
