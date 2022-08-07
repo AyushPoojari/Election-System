4aff76c855d6467d9a5339cf0d0ed269b22e854d
Nishant575 <n.dalvi2010@gmail.com>
2022-08-07 19:11:29 +0530

    fixed loading

--- src/js/app.js
+++ src/js/app.js
@@ -21,29 +21,61 @@ App = {
   },
 
   initContract: function() {
-    
-    
-    return App.bindEvents();
+    $.getJSON("Election.json", function(election) {
+
+      App.contracts.Election = TruffleContract(election);
+      App.contracts.Election.setProvider(App.web3Provider);
+
+      return App.render();
+    })
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
+  render: function() {
+    var electionInstance;
+    var loader = $("#loader");
+    var content = $("#content");
 
-  markAdopted: function() {
-    /*
-     * Replace me...
-     */
-  },
+    loader.show();
+    content.hide();
 
-  handleAdopt: function(event) {
-    event.preventDefault();
+    //load account data
+    web3.eth.getCoinbase(function(err,account) {
+      if(err == null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account:" + account);
+      }
+    });
 
-    var petId = parseInt($(event.target).data('id'));
+    App.contracts.Election.deployed().then(function(instance) {
+      electionInstance = instance;
+      return electionInstance.candidatesCount();
+    }).then(function(candidatesCount) {
+      var candidatesResults = $("#candidatesResults");
+      candidatesResults.empty();
 
-    /*
-     * Replace me...
-     */
+      var candidatesSelect = $('#candidatesSelect');
+      candidatesSelect.empty();
+
+      for (var i = 1; i <= candidatesCount; i++) {
+        electionInstance.candidates(i).then(function(candidate) {
+          var id = candidate[0];
+          var name = candidate[1];
+          var voteCount = candidate[2];
+
+          // Render candidate Result
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+          candidatesResults.append(candidateTemplate);
+
+          // Render candidate ballot option
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          candidatesSelect.append(candidateOption);
+        });
+      }
+      loader.hide();
+      content.show();
+    }).catch(function(error) {
+      console.warn(error);
+    });
   }
 
 };
